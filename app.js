1ec7411d53cf6dd3bd8a4d8f0aa220e5eae214e4
AllisonBasore <allison.basore@students.olin.edu>
2017-07-05 07:57:29 -0400

    d

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ var express = require('express');
 var index = require('./routes/index');
 var app = express();
 
+
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,6 @@ var express = require('express');
 var router = express.Router();
 var constants = require('../constants');
 
-
-var pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-
-
 //function that constructs and returns lizard object
 function Entry(id, score){
   var score = {
