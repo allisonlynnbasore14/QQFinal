2c9a80125c718a0dbb6ef58cbe3b6fdd53395dde
AllisonBasore <allison.basore@students.olin.edu>
2017-07-08 09:27:32 -0400

    d

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/db', function (request, response) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
-       { console.error(err); response.send("Error " + err); }
+       { console.error(err); response.send("Error HHHHH " + err); }
       else
        { response.render('pages/db', {results: result.rows} ); }
     });
