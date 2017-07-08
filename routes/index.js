4d969080f18ef31964df8c5accd8e72a2aefe050
AllisonBasore <allison.basore@students.olin.edu>
2017-07-08 11:04:15 -0400

    j

--- routes/index.js
+++ routes/index.js
@@ -24,10 +24,11 @@ var readingScores = function(req, res){
   res.render("readingScores", {"SCORE": [ msg ] } );
 };
 
-      console.log(process.env.DATABASE_URL, 'ppppppppppppppppppppppppppppppppppppppppppppppppppp')
 var home = function(req, res){
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
+
+      console.log(result, 'ppppppppppppppppppppppppppppppppppppppppppppppppppp')
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
