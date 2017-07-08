0c57fa2b2210ad95af5da40532bfd9d603280055
AllisonBasore <allison.basore@students.olin.edu>
2017-07-08 09:36:14 -0400

    f

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('/db', {results: result.rows} ); }
+       {app.get('/db', index.db); }
     });
   });
 });
