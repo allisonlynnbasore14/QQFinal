59656c025f97dfd299638645660f269ebb6ec4b9
AllisonBasore <allison.basore@students.olin.edu>
2017-07-08 12:31:39 -0400

    d

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ function Entry(id, score){
 var home = function(req, res){
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
-      console.log(result)
+      console.log(result, '99999999999999999999999999999999999999999999999999999')
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
