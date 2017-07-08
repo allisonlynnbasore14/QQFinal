2fbe76de11e0f5980cb92aad653af22d61a29a08
AllisonBasore <allison.basore@students.olin.edu>
2017-07-08 09:29:05 -0400

    d

--- app.js
+++ app.js
@@ -21,9 +21,10 @@ var pg = require('pg');
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
+    	console.log('Here jack')
       done();
       if (err)
-       { console.error(err); response.send("Error HHHHH " + err); }
+       { console.error(err); response.send("Error " + err); }
       else
        { response.render('pages/db', {results: result.rows} ); }
     });
