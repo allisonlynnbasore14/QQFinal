050ee4b5e1637f4baea566f76e3f21b841f29504
AllisonBasore <allison.basore@students.olin.edu>
2017-07-11 08:37:13 -0400

    dd

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ app.get('/', index.home);
 var router = express.Router();
 var constants = require('./constants');
 var pg = require('pg');
+// const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+
+const client = new pg.Client(connectionString);
+client.connect();
+// const query = client.query(
+//   'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+// query.on('end', () => { client.end(); });
 
 app.post('/login', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400)
