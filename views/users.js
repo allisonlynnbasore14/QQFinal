62c17ba3c18a7d22e36b4a9422ec16197eca74fb
AllisonBasore <allison.basore@students.olin.edu>
2017-07-10 22:40:56 -0400

    g

---
+++ views/users.js
@@ -0,0 +1,108 @@
+
+var users = module.exports = {
+    Euclid:{
+        id: 1,
+        description: 'Euclid, sometimes called Euclid of Alexandria to distinguish him from Euclides of Megara, was a Greek mathematician, often referred to as the "father of geometry". He was active in Alexandria during the reign of Ptolemy I.',
+        avatarUrl: 'http://www.thefamouspeople.com/profiles/images/euclid-3.jpg',
+    },
+    Newton:{
+        id: 2,
+        description: 'Sir Isaac Newton PRS was an English mathematician, astronomer, and physicist who is widely recognised as one of the most influential scientists of all time and a key figure in the scientific revolution',
+        avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/39/GodfreyKneller-IsaacNewton-1689.jpg',
+    },
+    Archimedes:{
+        id: 3,
+        description: 'Archimedes of Syracuse was a Greek mathematician, physicist, engineer, inventor, and astronomer. Although few details of his life are known, he is regarded as one of the leading scientists in classical antiquity.',
+        avatarUrl: 'https://vignette4.wikia.nocookie.net/39clues/images/8/80/Chocolate_card_600dpi.jpg/revision/latest?cb=20130319211527',
+    },
+    Gauss:{
+        id: 4,
+        description: 'Johann Carl Friedrich Gauss was a German mathematician who contributed significantly to many fields, including number theory, algebra, statistics, analysis, differential geometry, geodesy, geophysics, mechanics, electrostatics, astronomy, matrix theory, and optics.',
+        avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Carl_Friedrich_Gauss_1840_by_Jensen.jpg/1200px-Carl_Friedrich_Gauss_1840_by_Jensen.jpg',
+    },
+    Descartes:{
+        id: 5,
+        description: 'René Descartes was a French philosopher, mathematician, and scientist. Dubbed the father of modern western philosophy, much of subsequent Western philosophy is a response to his writings, which are studied closely to this day.',
+        avatarUrl: 'https://www.biography.com/.image/t_share/MTE1ODA0OTcxMjY3MzYwMjY5/rene-descartes-37613-1-402.jpg',
+    },
+    Fermat :{
+        id: 6,
+        description: 'Pierre de Fermat was a FrePierre de Fermat nch lawyer at the Parlement of Toulouse, France, and a mathematician who is given credit for early developments that led to infinitesimal calculus, including his technique of adequality.',
+        avatarUrl: 'https://media1.britannica.com/eb-media/03/11903-004-3101EDBE.jpg',
+    },
+    Euler :{
+        id: 7,
+        description: 'Leonhard Euler was a Swiss mathematician, physicist, astronomer, logician and engineer who made important and influential discoveries in many branches of mathematics like infinitesimal calculus and graph theory while also making pioneering contributions to several branches such as topology and analytic number theory.',
+        avatarUrl: 'http://1.bp.blogspot.com/-g5tHkrkX53g/T0t8SqmPqwI/AAAAAAAAP6c/pproHPXr_7A/s1600/3_.jpg',
+    },
+    Einstein :{
+        id: 8,
+        description: 'Albert Einstein was a German-born theoretical physicist. Einstein developed the theory of relativity, one of the two pillars of modern physics. Einsteins work is also known for its influence on the philosophy of science.',
+        avatarUrl: 'https://static.comicvine.com/uploads/scale_small/0/77/4359914-7-einstein-digital-art-digital-art-by-mark-fredrickson.jpg',
+    },
+    Leibniz :{
+        id: 9,
+        description: 'Gottfried Wilhelm Leibniz was a German polymath and philosopher who occupies a prominent place in the history of mathematics and the history of philosophy, having developed differential and integral calculus independently of Isaac Newton, his lifelong rival.',
+        avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/07/Leibniz_Hannover.jpg',
+    },
+    Hilbert :{
+        id: 10,
+        description: 'David Hilbert was a German mathematician. He is recognized as one of the most influential and universal mathematicians of the 19th and early 20th centuries.',
+        avatarUrl: 'https://i1.wp.com/short-biography.com/wp-content/uploads/david-hilbert/David-Hilbert.jpg?fit=324%2C324&ssl=1',
+    },
+    Pascal :{
+        id: 11,
+        description: 'Blaise Pascal was a French mathematician, physicist, inventor, writer and Catholic theologian. He was a child prodigy who was educated by his father, a tax collector in Rouen.',
+        avatarUrl: 'https://media1.britannica.com/eb-media/19/150319-004-9FAEC6DF.jpg',
+    },
+    Turing :{
+        id: 12,
+        description: 'Alan Mathison Turing was an English computer scientist, mathematician, logician, cryptanalyst, philosopher and theoretical biologist.',
+        avatarUrl: 'http://www.conservapedia.com/images/thumb/c/ca/Ture.jpg/300px-Ture.jpg',
+    },
+    Ramanujan :{
+        id: 13,
+        description: 'Srinivasa Iyengar Ramanujan FRS was an Indian mathematical genius and autodidact who lived during the British Raj.',
+        avatarUrl: 'https://s-media-cache-ak0.pinimg.com/originals/c5/70/82/c570820d0422735ce234e7b4fd19283b.jpg',
+    },
+    Riemann :{
+        id: 14,
+        description: 'Georg Friedrich Bernhard Riemann was a German mathematician who made contributions to analysis, number theory, and differential geometry. In the field of real analysis, he is mostly known for the first rigorous formulation of the integral, the Riemann integral, and his work on Fourier series.',
+        avatarUrl: 'http://www.thefamouspeople.com/profiles/images/bernhard-riemann-biography-4.jpg',
+    },
+    Neumann :{
+        id: 15,
+        description: 'John von Neumann was a Hungarian-American mathematician, physicist, inventor, computer scientist, and polymath. He made major contributions to a number of fields, including mathematics, physics, economics, computing, and statistics.',
+        avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/HD.3F.191_%2811239892036%29.jpg/220px-HD.3F.191_%2811239892036%29.jpg',
+    },
+    Ptolemy :{
+        id: 16,
+        description: 'Claudius Ptolemy was a Greek writer, known as a mathematician, astronomer, geographer, astrologer, and poet of a single epigram in the Greek Anthology. He lived in the city of Alexandria in the Roman province of Egypt, wrote in Koine Greek, and held Roman citizenship.',
+        avatarUrl: 'https://ptolemy.eecs.berkeley.edu/image/ptolemy.jpg',
+    },
+    Aryabhata :{
+        id: 17,
+        description: 'Aryabhata I was the first of the major mathematician-astronomers from the classical age of Indian mathematics and Indian astronomy. His works include the Āryabhaṭīya and the Arya-siddhanta.',
+        avatarUrl: 'http://www.thefamouspeople.com/profiles/images/aryabhata-4.jpg',
+    },
+    Cantor :{
+        id: 18,
+        description: 'Georg Ferdinand Ludwig Philipp Cantor was a German mathematician. He invented set theory, which has become a fundamental theory in mathematics.',
+        avatarUrl: 'http://www.thefamouspeople.com/profiles/images/georg-cantor-2.jpg',
+    },
+    Germain :{
+        id: 19,
+        description: 'Marie-Sophie Germain was a French mathematician, physicist, and philosopher. Despite initial opposition from her parents and difficulties presented by society, she gained education from books in her fathers library including ones by Leonhard Euler and from correspondence with famous mathematicians such as Lagrange, Legendre, and Gauss. One of the pioneers of elasticity theory, she won the grand prize from the Paris Academy of Sciences for her essay on the subject. Her work on Fermats Last Theorem provided a foundation for mathematicians exploring the subject for hundreds of years after.',
+        avatarUrl: 'http://www.heurema.com/PersonajesFQ/SophieGermain/GermainSophieN2-300px.jpg',
+    },
+    Ada :{
+        id: 20,
+        description: 'Augusta Ada King-Noel, Countess of Lovelace was an English mathematician and writer, chiefly known for her work on Charles Babbages proposed mechanical general-purpose computer, the Analytical Engine. She was the first to recognise that the machine had applications beyond pure calculation, and created the first algorithm intended to be carried out by such a machine. As a result, she is often regarded as the first to recognise the full potential of a "computing machine" and the first computer programmer',
+        avatarUrl: 'https://media.licdn.com/mpr/mpr/shrinknp_800_800/AAEAAQAAAAAAAAcTAAAAJGFjMWYzODdkLTU0ODQtNDVhNS1hNzQ3LTk2YmNlYmRjOWI1ZA.jpg',
+    },
+     Noether :{
+        id: 21,
+        description: 'Amalie Emmy Noether was a German mathematician known for her landmark contributions to abstract algebra and theoretical physics. She was described by Pavel Alexandrov, Albert Einstein, Jean Dieudonné, Hermann Weyl, and Norbert Wiener as the most important woman in the history of mathematics.[2][3] As one of the leading mathematicians of her time, she developed the theories of rings, fields, and algebras. In physics, Noethers theorem explains the connection between symmetry and conservation laws.',
+        avatarUrl: 'https://light2015blogdotorg.files.wordpress.com/2015/10/pic1-emmynoether.jpg',
+    }
+}
