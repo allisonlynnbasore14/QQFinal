529a7f2c5879ee702e7a669e407e20445c161cb6
AllisonBasore <allison.basore@students.olin.edu>
2017-07-12 07:13:11 -0400

    ff

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.post('/login', urlencodedParser, function (req, res) {
       console.log(err);
       return res.status(500).json({success: false, data: err});
     }
-    client.query("SELECT * FROM users WHERE user_id = '1'", function(err, result) {
+    client.query("SELECT * FROM users WHERE user_id = 1", function(err, result) {
     	var status = result.row[0].status;
     	console.log(status)
 	  // OR 'name: %s', result.rows[0]['status']);
