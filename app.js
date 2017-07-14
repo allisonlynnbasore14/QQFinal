bf196b4db0750746b806e9a645220bfaac5fd878
AllisonBasore <allison.basore@students.olin.edu>
2017-07-14 06:46:18 -0400

    ff

--- app.js
+++ app.js
@@ -47,12 +47,13 @@ app.post('/login', urlencodedParser, function (req, res) {
       return res.status(500).json({success: false, data: err});
       }
       client.query('SELECT * FROM users WHERE user_id=$1', [id] , function(err, result) {
-  	   var status = result.rows[0].status;
+        done()
+  	     var status = result.rows[0].status;
+        console.log(status, 'MADE ITIIIIIIIIIIIIIIIIIIIIIIIIIIIIIi')
+        app.get("/home", index.home)
       })
 	  });
     //res.render("home",{"directions": constants.DIR.HOME, "title": constants.TITLE.HOM, "status":status, "id": id});
-    console.log(status, 'MADE ITIIIIIIIIIIIIIIIIIIIIIIIIIIIIIi')
-  app.get("/home", index.home)
   };
 })
 
