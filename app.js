8b337ae4587b8d3b03c7fd1466290e95ce63e9ec
AllisonBasore <allison.basore@students.olin.edu>
2017-07-14 07:01:05 -0400

    ff

--- app.js
+++ app.js
@@ -49,8 +49,9 @@ app.post('/login', urlencodedParser, function (req, res) {
       client.query('SELECT * FROM users WHERE user_id=$1', [id] , function(err, result) {
         done()
   	     var status = result.rows[0].status;
-          res.render("home",{"directions": constants.DIR.HOME, "title": constants.TITLE.HOM, "status": status});
+          //res.render("home",{"directions": constants.DIR.HOME, "title": constants.TITLE.HOM, "status": status});
           app.get('/home', index.home);
+          res.render("home",{"directions": constants.DIR.HOME, "title": constants.TITLE.HOM, "status": status});
         // if (req.query.StatusUpdate == 'KEY'){
         //   unLockQuiz(status)
         // }
