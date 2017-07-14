5d0a09a9328d91f9150ffc9efb9932b7907f709a
AllisonBasore <allison.basore@students.olin.edu>
2017-07-14 08:00:43 -0400

    ff

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.post('/login', urlencodedParser, function (req, res, next) {
   const status = 3;
   res.render("home",{"directions": constants.DIR.HOME, "title": constants.TITLE.HOM, "status": status});
   //if (req.query.StatusUpdate == 100){
-    console.log(req.query.StatusUpdate, 'kkkkk')
+    console.log(req, 'kkkkk')
     unLockQuiz(status)
   //}
 }
