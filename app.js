8ac7ea32f7487c70aa012bb8344a6ba6e5d12f44
AllisonBasore <allison.basore@students.olin.edu>
2017-07-15 08:45:19 -0400

    ggg

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', index.home);
+//app.get('/', index.home);
 
 
 var router = express.Router();
