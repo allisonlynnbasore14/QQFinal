e0a8825c3bc5d5e8fbbc3a8ef192a9ea99fbfa9d
AllisonBasore <allison.basore@students.olin.edu>
2017-07-16 15:02:18 -0400

    dd

--- routes/index.js
+++ routes/index.js
@@ -139,7 +139,7 @@ function sendToDB(newStatus, id){
       return res.status(500).json({success: false, data: err});
       }
             console.log('yyyyyyyyyyyyyyyyyyyyyyyyy')
-           client.query('UPDATE users SET status=0 WHERE user_id=2', function(err, result) {
+           client.query('UPDATE users SET status=$1 WHERE user_id=$2', [newStatus, id] , function(err, result) {
         done()
       })
     });
